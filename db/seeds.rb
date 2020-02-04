9de2219f82f7cfded6396de3b58373d5cd4bcd6b
mykle jones <mykle.j91@gmail.com>
2020-02-04 12:43:55 -0500

    anime added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Subject.destroy_all
+
 Anime.destroy_all
 User.destroy_all
 
 
-Subject.create({name: "Anime", description: "Everything Anime", photo_url:"https://images5.alphacoders.com/658/thumb-350-658649.jpg"})
-Subject.create({name: "Coder", description: "Everything Code", photo_url:"https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"})
 
 Anime.create({name:"Naruto" ,publish_date:"October 3, 2002" , rating: 5 , photo_url: "https://wallpaperaccess.com/full/229954.jpg", description:"Naruto (ナルト) is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, a young ninja who seeks to gain recognition from his peers and also dreams of becoming the Hokage, the leader of his village."})
 Anime.create({name:"Black Clover" ,publish_date:"October 3, 2017" , rating: 4.5, photo_url: "https://i.pinimg.com/originals/70/6b/8e/706b8e922df2e05504d8dc5f6e042d2d.png", description:"In a world where people are born with magic, Asta was born without any. In contrast, Yuno was born a prodigy with above average magic power and the talent to control it. Additionally, in their coming of age ceremony, Yuno receives a rare four-leaf clover grimoire, like the original Magic Emperor."})
 Anime.create({name:"Dragon ball Z" ,publish_date:"April 26, 1989" , rating: 5, photo_url: "https://img.20mn.fr/cV3LuwV7TMuC3kydQMHbQA/310x190_dragon-ball-super-nouvelle-serie-suite-directe-dragon-ball-z.jpg", description:"Dragon Ball Z follows the adventures of Goku who, along with the Z Warriors, defends the Earth against evil. The action adventures are entertaining and reinforce the concept of good versus evil. 'Dragon Ball Z' teaches valuable character virtues such as teamwork, loyalty, and trustworthiness."})
+Anime.create({name:"Bleach" ,publish_date:"August 7, 2001" , rating: 5, photo_url: "https://www.gamerbraves.com/wp-content/uploads/2018/10/Bleach-Awakened-Souls-feature-image.jpg", description:"The series aired on TV Tokyo from October 2004 to March 2012, spanning 366 episodes. The story follows the adventures of Ichigo Kurosaki after he obtains the powers of a Soul Reaper (死神, Shinigami, literally, 'Death God') — a death personification similar to the Grim Reaper — from another Soul Reaper, Rukia Kuchiki."})
+Anime.create({name:"My Hero Academia" ,publish_date:"April 3, 2016" , rating: 5, photo_url: "https://i2.wp.com/asianretrospects.com/wp-content/uploads/2018/07/My-Hero-Academia-Poster.jpg?fit=1920%2C1080&ssl=1", description:"Boku no Hīrō Akademia) is a Japanese superhero manga series written and illustrated by Kōhei Horikoshi. It has been serialized in Weekly Shōnen Jump since July 2014, with its chapters additionally collected in 24 tankōbon volumes as of August 2019"})
 
 
 User.create({user_name:"mykle", password:"password"})
